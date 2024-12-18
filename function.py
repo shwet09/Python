d784ada653e3e7177f06a48a3e50e9c0fe40c9bd
Shweta <shwetkushwaha009@gmail.com>
2024-12-18 21:13:01 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,8 @@
+# functions are invoked nd used on again a again loop  in a shorter way 
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}! ")
+    print(f"You are {age} old!")
+
+happy_birthday("Bro", "20")
+happy_birthday("Diya", "12")
